785517e0402a31156b16c6af1ad73064ddde8968
mob-sakai <sakai861104@gmail.com>
2020-11-09 14:07:29 +0900

    build: 1.3.0

---
+++ Assets/OpenSesame/IgnoreAccessibility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace OpenSesameTests
+{
+    public class IgnoreAccessibility : MonoBehaviour
+    {
+        private void Start()
+        {
+            var text = GetComponent<Text>();
+#if IGNORE_ACCESSIBILITY
+            var list = ListPool<int>.Get();
+
+            list.Add(1);
+            list.Add(2);
+
+            ListPool<int>.Release(list);
+
+            text.text = $"IGNORE_ACCESSIBILITY is included.";
+            text.color = Color.green;
+#else
+            text.text = $"IGNORE_ACCESSIBILITY is not included.";
+            text.color = Color.red;
+#endif
+        }
+    }
+}
