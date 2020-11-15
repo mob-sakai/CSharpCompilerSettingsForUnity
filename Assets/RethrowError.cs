d34a44005b76c1c7d43257dcef946ececcb70ec7
mob-sakai <sakai861104@gmail.com>
2020-11-15 22:31:51 +0900

    update

---
+++ Assets/RethrowError.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class RethrowError : MonoBehaviour
+{
+    void Update()
+    {
+        try
+        {
+            DoSomethingInteresting();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            throw e;
+        }
+    }
+
+    private void DoSomethingInteresting()
+    {
+        throw new System.NotImplementedException();
+    }
+}
