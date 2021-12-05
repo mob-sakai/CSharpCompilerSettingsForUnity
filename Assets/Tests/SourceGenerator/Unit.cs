70dc442ff8f7e5101f6cdeba81d06bfd4363cc29
mob-sakai <sakai861104@gmail.com>
2021-12-06 02:19:02 +0900

    test: add source generator tests

---
+++ Assets/Tests/SourceGenerator/Unit.cs
@@ -0,0 +1,26 @@
+﻿#if CUSTOM_COMPILE
+using NUnit.Framework;
+
+namespace SourceGenerator
+{
+    public class Unit
+    {
+        [Test]
+        public void UnitToString()
+        {
+            var userId = new UserId(1234);
+            Assert.AreEqual("UserId(1234)", userId.ToString());
+        }
+
+        [Test]
+        public void AsPrimitive()
+        {
+            var userId = new UserId(1234);
+            Assert.AreEqual(1234, userId.AsPrimitive());
+        }
+    }
+
+    [UnitGenerator.UnitOf(typeof(int))]
+    public readonly partial struct UserId { }
+}
+#endif
